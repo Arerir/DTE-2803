4de43514d8eb5b7fbb1832c2f16b723881898ddd
Daniel Eli <dee.00123@gmail.com>
2020-10-25 11:04:38 +0100

    Adding DTO classes

---
+++ EventMessenger/RESTServer/Data/DTO/BadEventDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTServer.Data.DTO
+{
+    public class BadEventDTO
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Placement { get; set; }
+        public DateTime Date { get; set; }
+        public int SeverityId { get; set; }
+        public int StatusId { get; set; }
+    }
+}
---
+++ EventMessenger/RESTServer/Data/DTO/ReminderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTServer.Data.DTO
+{
+    public class ReminderDTO
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+    }
+}

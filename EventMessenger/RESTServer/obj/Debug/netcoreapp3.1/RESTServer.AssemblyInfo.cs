7abb9a2797f740cd342f2b68b46629971ba1f775
Daniel Eli <dee.00123@gmail.com>
2020-10-25 10:49:20 +0100

    Configurated deployment

--- EventMessenger/RESTServer/obj/Debug/netcoreapp3.1/RESTServer.AssemblyInfo.cs
+++ EventMessenger/RESTServer/obj/Debug/netcoreapp3.1/RESTServer.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a72e622b-40ab-4974-903e-63065bf4ca67")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

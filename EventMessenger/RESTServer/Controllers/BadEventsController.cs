58abc9b661f3adc24dc283fd9c94b37402dfa569
Daniel Eli <dee.00123@gmail.com>
2020-11-08 16:37:23 +0100

    Update BadEvents to use DTO #2

--- EventMessenger/RESTServer/Controllers/BadEventsController.cs
+++ EventMessenger/RESTServer/Controllers/BadEventsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RESTServer.Data;
+using RESTServer.Data.DTO;
 using RESTServer.Entities;
 
 namespace RESTServer.Controllers
@@ -23,15 +24,30 @@ namespace RESTServer.Controllers
 
         // GET: api/BadEvents
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<BadEvent>>> GetEvents()
+        public async Task<ActionResult<IEnumerable<BadEventDTO>>> GetEvents()
         {
-            return await _context.Events.ToListAsync();
+            // if user is authenticated
+
+            //limit list by user.HasAdmin ? All : .Where(x => x.CreatedBy == user.Id)
+
+            var list = await _context.Events.ToListAsync();
+
+            var dtoList = new List<BadEventDTO>();
+
+            foreach (var item in list)
+            {
+                dtoList.Add(BadEventDTO.Selector().Compile()(item));
+            }
+
+            return dtoList;
         }
 
         // GET: api/BadEvents/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<BadEvent>> GetBadEvent(int id)
+        public async Task<ActionResult<BadEventDTO>> GetBadEvent(int id)
         {
+            // check user Access
+
             var badEvent = await _context.Events.FindAsync(id);
 
             if (badEvent == null)
@@ -39,21 +55,75 @@ namespace RESTServer.Controllers
                 return NotFound();
             }
 
-            return badEvent;
+            return BadEventDTO.Selector().Compile()(badEvent);
         }
 
         // PUT: api/BadEvents/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutBadEvent(int id, BadEvent badEvent)
+        public async Task<IActionResult> PutBadEvent(int id, BadEventDTO dto)
         {
-            if (id != badEvent.Id)
+            // check user Access
+            if (id != dto.Id)
             {
                 return BadRequest();
             }
 
+            var badEvent = _context.Events.FirstOrDefault(x => x.Id == id);
+
+            if(badEvent == null)
+            {
+                return NotFound();
+            }
+
+            badEvent.Date = dto.Date;
+            badEvent.Message = dto.Message;
+            badEvent.Placement = dto.Placement;
+            badEvent.Reason = dto.Reason;
+            badEvent.SeverityId = dto.SeverityId;
+            badEvent.StatusId = dto.StatusId;
+            badEvent.Modified = DateTime.Now;
+            //badEvent.ModifiedBy = user.Id;
+
             _context.Entry(badEvent).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BadEventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPut("archive/{id}")]
+        public async Task<IActionResult> PutArchive(int id, BadEventDTO dto)
+        {
+            // check user Access
+            if (id != dto.Id)
+            {
+                return BadRequest();
+            }
+
+            var badEvent = _context.Events.FirstOrDefault(x => x.Id == id);
+
+            if (badEvent == null)
+            {
+                return NotFound();
+            }
+
+            badEvent.Archived = true;
 
             try
             {
@@ -78,8 +148,21 @@ namespace RESTServer.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<BadEvent>> PostBadEvent(BadEvent badEvent)
+        public async Task<ActionResult<BadEventDTO>> PostBadEvent(BadEventDTO dto)
         {
+
+            var badEvent = new BadEvent()
+            {
+                Created = DateTime.Now,
+                Date = dto.Date,
+                IsDeleted = false,
+                Message = dto.Message,
+                Placement = dto.Placement,
+                Reason = dto.Reason,
+                SeverityId = dto.SeverityId,
+                StatusId = 1
+            };
+
             _context.Events.Add(badEvent);
             await _context.SaveChangesAsync();
 
@@ -88,7 +171,7 @@ namespace RESTServer.Controllers
 
         // DELETE: api/BadEvents/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<BadEvent>> DeleteBadEvent(int id)
+        public async Task<ActionResult<bool>> DeleteBadEvent(int id)
         {
             var badEvent = await _context.Events.FindAsync(id);
             if (badEvent == null)
@@ -96,10 +179,10 @@ namespace RESTServer.Controllers
                 return NotFound();
             }
 
-            _context.Events.Remove(badEvent);
+            badEvent.IsDeleted = true;
             await _context.SaveChangesAsync();
 
-            return badEvent;
+            return true;
         }
 
         private bool BadEventExists(int id)
--- EventMessenger/RESTServer/Data/DTO/BadEventDTO.cs
+++ EventMessenger/RESTServer/Data/DTO/BadEventDTO.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RESTServer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace RESTServer.Data.DTO
@@ -14,5 +16,19 @@ namespace RESTServer.Data.DTO
         public DateTime Date { get; set; }
         public int SeverityId { get; set; }
         public int StatusId { get; set; }
+
+        public static Expression<Func<BadEvent, BadEventDTO>> Selector()
+        {
+            return x => new BadEventDTO
+            {
+                Id = x.Id,
+                Message = x.Message,
+                Reason = x.Reason,
+                Placement = x.Placement,
+                Date = x.Date,
+                SeverityId = x.SeverityId,
+                StatusId = x.StatusId
+            };
+        }
     }
 }

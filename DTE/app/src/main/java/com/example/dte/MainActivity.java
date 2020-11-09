d6aa552acef2cae91e1d6a939e5026fd8d79ccd9
Iuliia <iuk001@uit.no>
2020-11-09 21:06:08 +0100

    skrin view og jobb med funksjon til overføring appi foresporsel

--- DTE/app/src/main/java/com/example/dte/MainActivity.java
+++ DTE/app/src/main/java/com/example/dte/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.dte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -15,57 +17,176 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
+
+
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.Buffer;
+
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final Properties props = System.getProperties();
+        props.setProperty("jdk.internal.httpclient.disableHostnameVerification", Boolean.TRUE.toString());
     }
     public void onClickBtn(View v)
     {
+
+        EditText txt1 = (EditText) findViewById(R.id.name);
+        EditText txt2 = (EditText) findViewById(R.id.password);
+        Map<String, String> login = new HashMap<String, String>();
+        login.put("userId", txt1.getText().toString());
+        login.put("password", txt2.getText().toString());
+        String s = makeRequest("https://danieleli2.asuscomm.com/api/users/authenticate", login);
+//        JsonObject convertedObject = new Gson().fromJson(s, JsonObject.class);
+//
+//        System.out.println(convertedObject.toString());
         Intent intent = new Intent(MainActivity.this, NavigationEvents.class);
         startActivity(intent);
-
-        Map<String, String> comment = new HashMap<String, String>();
-//        comment.put("subject", "Using the GSON library");
-//        comment.put("message", "Using libraries is convenient.");
-        String json = new GsonBuilder().create().toJson(comment, Map.class);
-//        makeRequest("http://192.168.0.1:3000/post/77/comments", json);
     }
 
-    public static HttpResponse makeRequest(String uri, String json) {
+    public static String makeRequest(final String uri, final Map<String, String> obj) {
+        final String[] value = new String[1];
+        Thread t = new Thread() {
+
+            public void run() {
+                Looper.prepare(); //For Preparing Message Pool for the child Thread
+                OkHttpClient client = new OkHttpClient();
+
+
+
+                
+                RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("userId", obj.get("userId"))
+                        .addFormDataPart("password", obj.get("password"))
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url(uri)
+                        .post(requestBody)
+                        .build();
+
+
+//                DefaultHttpClient client = new DefaultHttpClient();
+//                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+//                HttpResponse response;
+//                JSONObject json = new JSONObject();
+//
+//                try {
+//                    HttpPost post = new HttpPost(uri);
+//                    for (String i : obj.keySet()) {
+//                        json.put(i, obj.get(i));
+//                    }
+//
+//                    StringEntity se = new StringEntity(json.toString());
+//                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+//                    post.setEntity(se);
+//                    response = client.execute(post);
+//
+//                    HttpEntity entity = response.getEntity();
+//                    String responseString = EntityUtils.toString(entity, "UTF-8");
+//                Buffer buffer = new Buffer();
+//                try {
+//                    request.body().writeTo(buffer);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+                
+                Response response = null;
+                try {
+                    response = client.newCall(request).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(response.body().toString());
+//                } catch(Exception e) {
+//                    e.printStackTrace();
+//                    System.out.println(e);
+//
+//                }
+
+                Looper.loop(); //Loop in the message queue
+            }
+        };
+
+        t.start();
         try {
-            HttpPost httpPost = new HttpPost(uri);
-            httpPost.setEntity(new StringEntity(json));
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-            return new DefaultHttpClient().execute(httpPost);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            Thread.sleep(1500);
+        } catch(InterruptedException e) {
+            System.out.println("got interrupted!");
         }
-        return null;
+        return value[0];
     }
+
+
+//    public static String makeRequest(final String uri) {
+//        final String[] value = new String[1];
+//        Thread t = new Thread() {
+//
+//            public void run() {
+//                Looper.prepare(); //For Preparing Message Pool for the child Thread
+//                HttpClient client = new DefaultHttpClient();
+//                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+//                HttpResponse response;
+//
+//                try {
+//                    HttpGet get = new HttpGet(uri);
+//                    response = client.execute(get);
+//                    HttpEntity entity = response.getEntity();
+//                    String responseString = EntityUtils.toString(entity, "UTF-8");
+//                    value[0] = responseString;
+//                } catch(Exception e) {
+//                    e.printStackTrace();
+//                    System.out.println(e);
+//
+//                }
+//
+//                Looper.loop(); //Loop in the message queue
+//            }
+//        };
+//
+//        t.start();
+//        try {
+//            Thread.sleep(1500);
+//        } catch(InterruptedException e) {
+//            System.out.println("got interrupted!");
+//        }
+//        return value[0];
+//    }
+
     public void onClickRegister(View v){
         Intent intent = new Intent(MainActivity.this, register.class);
         startActivity(intent);
--- DTE/app/src/main/java/com/example/dte/register.java
+++ DTE/app/src/main/java/com/example/dte/register.java
@@ -2,7 +2,27 @@ package com.example.dte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
+import android.widget.EditText;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
 
@@ -10,5 +30,105 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void onClickBtn(View v)
+    {
+
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText password = (EditText) findViewById(R.id.password);
+        EditText firstName = (EditText) findViewById(R.id.fødselsnummer);
+        EditText email = (EditText) findViewById(R.id.epostadressen);
+        Map<String, String> reg = new HashMap<String, String>();
+        reg.put("email", email.getText().toString());
+        reg.put("password", password.getText().toString());
+        reg.put("firstName", firstName.getText().toString());
+        reg.put("birthId", name.getText().toString());
+
+        String s = makeRequest("https://danieleli2.asuscomm.com/api/users", reg);
+
+    }
+
+    public static String makeRequest(final String uri, final Map<String, String> obj) {
+        final String[] value = new String[1];
+        Thread t = new Thread() {
+
+            public void run() {
+                Looper.prepare(); //For Preparing Message Pool for the child Thread
+
+
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                HttpResponse response;
+                JSONObject json = new JSONObject();
+
+                try {
+                    HttpPost post = new HttpPost(uri);
+                    for (String i : obj.keySet()) {
+                        json.put(i, obj.get(i));
+                    }
+
+                    StringEntity se = new StringEntity(json.toString());
+                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    post.setEntity(se);
+                    response = client.execute(post);
+
+                    HttpEntity entity = response.getEntity();
+                    String responseString = EntityUtils.toString(entity, "UTF-8");
+                    value[0] = responseString;
+                    System.out.println(responseString);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.out.println(e);
+
+                }
+
+                Looper.loop(); //Loop in the message queue
+            }
+        };
+
+        t.start();
+        try {
+            Thread.sleep(1500);
+        } catch(InterruptedException e) {
+            System.out.println("got interrupted!");
+        }
+        return value[0];
+    }
+
+
+    public static String makeRequest(final String uri) {
+        final String[] value = new String[1];
+        Thread t = new Thread() {
+
+            public void run() {
+                Looper.prepare(); //For Preparing Message Pool for the child Thread
+                HttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                HttpResponse response;
+
+                try {
+                    HttpGet get = new HttpGet(uri);
+                    response = client.execute(get);
+                    HttpEntity entity = response.getEntity();
+                    String responseString = EntityUtils.toString(entity, "UTF-8");
+                    value[0] = responseString;
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    System.out.println(e);
+
+                }
+
+                Looper.loop(); //Loop in the message queue
+            }
+        };
+
+        t.start();
+        try {
+            Thread.sleep(1500);
+        } catch(InterruptedException e) {
+            System.out.println("got interrupted!");
+        }
+        return value[0];
     }
 }

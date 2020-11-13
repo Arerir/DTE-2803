11bb310889b81a7a142751d21715abbf4dea160b
Avixon <eskil.avixon@gmail.com>
2020-11-13 01:30:44 +0100

    Reverted some changes from previous commit
    showevents, dataadapter, gradle file

--- DTE/app/src/main/java/com/example/dte/ShowEvents.java
+++ DTE/app/src/main/java/com/example/dte/ShowEvents.java
@@ -1,38 +1,58 @@
 package com.example.dte;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.dte.APICalls.Call;
+import com.example.dte.APICalls.Helper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.util.ArrayList;
+import org.json.JSONArray;
+
+import java.lang.reflect.Type;
 import java.util.List;
 
-public class ShowEvents extends AppCompatActivity {
 
-    List<Events> phones = new ArrayList<>();
+public class ShowEvents extends AppCompatActivity {
+    private static final String TAG = "ShowEvents";
+    private Call call;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_events);
 
-        setInitialData();
+        // Init recyclerview and adapter
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        // создаем адаптер
-        DataAdapter adapter = new DataAdapter(this, phones);
-        // устанавливаем для списка адаптер
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
-    private void setInitialData(){
-
-        phones.add(new Events("Stor hendelse", "01.10.2020", "10"));
-        phones.add(new Events("Liten hendelse", "02.10.2020","2"));
-        phones.add(new Events("Middle hendelse", "05.10.2020", "5"));
+        Helper.nuke();
+        call = Call.getInstance(getCacheDir());
+        call.getBadEvents(new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, "SUCCESS: " + response.toString());
+                Gson gson = new Gson();
+                Type listType = new TypeToken<List<BadEvent>>() {}.getType();
+                List<BadEvent> events = gson.fromJson(response.toString(), listType);
+                DataAdapter adapter = new DataAdapter(getApplicationContext(), events);
+                recyclerView.setAdapter(adapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "ERROR: " + error.getMessage());
+            }
+        });
     }
     public void onClickBtn1(View v)
     {

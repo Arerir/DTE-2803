2165608452c6ed5527bbd6e73d937f05ebe37fbf
Avixon <eskil.avixon@gmail.com>
2020-11-17 01:55:52 +0100

    Android: Can now see info about specific event from events list view
    Not all relevant information can be seen atm. because of missing API calls in asp net

--- DTE/app/src/main/java/com/example/dte/DataAdapter.java
+++ DTE/app/src/main/java/com/example/dte/DataAdapter.java
@@ -1,9 +1,12 @@
 package com.example.dte;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -47,8 +50,10 @@ class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView nameView, timeView, alvorlighetsgradView;
+        LinearLayout root;
         ViewHolder(View view){
             super(view);
+            root = (LinearLayout) view.findViewById(R.id.rootLinearLayout);
             nameView = (TextView) view.findViewById(R.id.name);
             timeView = (TextView) view.findViewById(R.id.time);
             alvorlighetsgradView = (TextView) view.findViewById(R.id.alvorlighetsgrad);
@@ -58,6 +63,19 @@ class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
             nameView.setText(String.valueOf(badEvent.getMessage()));
             timeView.setText(String.valueOf(badEvent.getDate()));
             alvorlighetsgradView.setText(String.valueOf(badEvent.getSeverityId()));
+
+            // Onclick open ViewEvent to display selected badEvent
+            root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, ViewEvent.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    Bundle b = new Bundle();
+                    b.putParcelable("badEvent", badEvent);
+                    intent.putExtras(b);
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 }
